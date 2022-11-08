65cfba381178627b21f2545af6ceb661fa545dba
Ecommerce WebDev <wellington.lima6@aluno.senai.br>
2022-11-08 10:33:02 -0300

    update categoria

--- src/Components/CategoriasEditar/index.jsx
+++ src/Components/CategoriasEditar/index.jsx
@@ -3,22 +3,49 @@ import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import { Form } from 'react-bootstrap';
 import './style.css';
+import { ecommerceAPI } from "../../Services";
+
 
 const AdminCategoriaEditar = () => {
+    const [nome, setNome] = useState(null)
+    const [descricao, setDescricao] = useState(null)
+    const [id, setId] = useState(null)
+    
+    function getId(val) {
+      setId(val.target.value)
+    }
+
+    function getNome(val) {
+      setNome(val.target.value)
+    }
+
+    function getDescricao(val) {
+      setDescricao(val.target.value)
+    }
+    
+    const novaCategoria = {nome: nome, descricao: descricao};
+    const updateCategoria = (e) => {
+      e.preventDefault();
+      ecommerceAPI.put(`/categoria/${id}`, novaCategoria).then(res => console.log("Updating...", res)).catch(err => console.log(err))
+    }
 
     return (
       <>
       <Container>
         <Form>
           <div class="row">
+            <label class="texto">Id Categoria:</label>
+            <input type="number" onChange={getId}/>
+          </div>
+          <div class="row">
             <label class="texto">Nome:</label>
-            <input type="text" />
+            <input type="text" onChange={getNome}/>
           </div>
           <div class="row">
             <label>Descrição:</label>
-            <textarea type="text" />
+            <textarea type="text" onChange={getDescricao}/>
           </div>
-          <button type="button" class="button-options-cadastrar">Salvar</button>
+          <button type="button" class="button-options-cadastrar" onClick={updateCategoria}>Salvar</button>
         </Form>
       </Container>
       </>
